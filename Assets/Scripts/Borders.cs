a25c51a95c51b4f5cb7b471d453b2c99de009e9a
pewpewbs <sviatoslav1423@gmail.com>
2023-02-21 18:03:54 +0200

    SnakeVersion_1.0
    First version

---
+++ Assets/Scripts/Borders.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Borders : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SnakeMain"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SnakeMain"))
+        {
+            
+            other.GetComponent<SnakeMovment>().AddTail();
+            Destroy(gameObject);
+        }
+    }
+}
